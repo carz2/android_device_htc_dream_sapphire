f7040c75c48d8e114685e671c56bfe478f191feb
carz <carz8500@gmail.com>
2011-08-19 22:42:57 -0600

    don't build trout sensors, we are sapphire only

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -40,27 +40,4 @@ LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
 
-# HAL module implemenation, not prelinked and stored in
-# hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := sensors.trout
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\"
-LOCAL_SRC_FILES := \
-    sensors.c               \
-    nusensors.cpp           \
-    InputEventReader.cpp    \
-    SensorBase.cpp          \
-    AkmSensor.cpp
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_PRELINK_MODULE := false
-
-include $(BUILD_SHARED_LIBRARY)
-
 endif # !TARGET_SIMULATOR
